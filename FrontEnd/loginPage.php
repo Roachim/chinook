fb27bd54bebdd2e399c9af2a5f06530991ff4949
Roachim <78924571+Roachim@users.noreply.github.com>
2021-12-13 09:14:01 +0100

    dont remember

--- FrontEnd/loginPage.php
+++ FrontEnd/loginPage.php
@@ -1,3 +1,40 @@
+<?php
+    session_start();
+
+    $userValidation = false;    
+
+    // If the user has clicked on 'Logout', the session is destroyed
+    if (isset($_POST['logout'])) { 
+        session_destroy();
+    
+    // If the user is already logged in, s/he is redirected to the search page
+    } else if (isset($_SESSION['userID'])) {    
+        header('Location: index.php');
+
+    // If the user has filled the login fields, the authentication process is launched
+    } else if (isset($_POST['loginEmail']) && isset($_POST['loginPass'])) {
+        
+        $userValidation = true;
+        require_once('customer.php');
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        
+        $validUser = $user->validate($email, $password);
+        if ($validUser) {
+            session_start();
+
+            $_SESSION['userID'] = $user->userID;
+            $_SESSION['firstName'] = $user->firstName;
+            $_SESSION['lastName'] = $user->lastName;
+            $_SESSION['email'] = $email;
+
+            header('Location: browsePage.php');
+        }
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,7 +63,7 @@
     <footer>
         <tr>Don't have an account?</tr>
         <br>
-        <button id="AccountCreate"></button>
+        <a href="createUser.php">Sign Up Here</a>
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>

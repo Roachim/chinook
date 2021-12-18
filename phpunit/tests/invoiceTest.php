420fa33a799fdeab120fd673798093b50062872d
Roachim <78924571+Roachim@users.noreply.github.com>
2021-12-18 22:27:30 +0100

    new test

---
+++ phpunit/tests/invoiceTest.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'API/invoice.php';
+use PHPUnit\Framework\TestCase;
+class dbTest extends TestCase{
+
+    public function test_Create_True(){
+        //arrange
+        $invoice = new Invoice();
+
+        $customerid = 1;
+        $billingAddress = 'asdsaf';
+        $billingCity = 'asfasdg';
+        $billingState = 'asfa';
+        $billingCountry = 'asfa';
+        $billingPostal = 'asf';
+        $total = 0.99;
+        $itemArray = ["1","2","3"];
+        
+        //act
+        $result = $invoice->Create($customerid,$billingAddress, $billingCity, $billingState, $billingCountry, $billingPostal, $total, $itemArray);
+        //assert
+        $this->assertTrue($result, "The result should be not null");
+    }
+    public function test_Create_False(){
+        //arrange
+        $invoice = new Invoice();
+
+        $customerid = null;
+        $billingAddress = 'asdsaf';
+        $billingCity = 'asfasdg';
+        $billingState = 'asfa';
+        $billingCountry = 'asfa';
+        $billingPostal = 'asf';
+        $total = 0.99;
+        $itemArray = ["1","2","3"];
+        
+        //act
+        $result = $invoice->Create($customerid,$billingAddress, $billingCity, $billingState, $billingCountry, $billingPostal, $total, $itemArray);
+        //assert
+        $this->assertTrue($result, "The result should be null");
+    }
+}
+
+?>
